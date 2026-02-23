22646ff5f4c988f87b73a0ec264a500ef2cb665e
ddnoergaard <ddnoergaard@gmail.com>
2026-02-23 22:44:27 +0100

    Add project files.

---
+++ Lockhub/wwwroot/js/navbar-mobile.js
@@ -0,0 +1,23 @@
+﻿function toggleMobileNav() {
+    const mobileNav = document.querySelector('.hidden-mobile-nav');
+
+    let overlay = document.querySelector('.nav-overlay');
+    if (!overlay) {
+        overlay = document.createElement('div');
+        overlay.classList.add('nav-overlay');
+        overlay.addEventListener('click', toggleMobileNav);
+        document.body.appendChild(overlay);
+    }
+
+    mobileNav.classList.toggle('nav-open');
+    overlay.classList.toggle('overlay-active');
+}
+
+document.querySelector('.nav-list-mobile').addEventListener('click', toggleMobileNav);
+document.querySelector('.hidden-mobile-nav img').addEventListener('click', toggleMobileNav);
+
+function toggleServices(e) {
+    e.preventDefault();
+    console.log("test")
+    document.querySelector('.mobile-services-on-hover').classList.toggle('services-open');
+}

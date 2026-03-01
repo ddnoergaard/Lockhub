0c6bcf88f4e338bb9d2ba2231ce5801f2ca69a9f
ddnoergaard <ddnoergaard@gmail.com>
2026-03-01 22:33:53 +0100

    Started on the _AppLayout Found new colors the website should have - primarily green jwt still working - nice

---
+++ Lockhub/Pages/Shared/_AppLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lockhub.Pages.Shared
+{
+    public class _AppLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
